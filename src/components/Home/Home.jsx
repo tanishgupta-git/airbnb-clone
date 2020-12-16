dfa3e5ea9bc530589d093c1f613300d228ff9d93
tanishgupta-git <tanishgupta8181@gmail.com>
2020-12-16 15:02:50 +0530

    added initial files

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Banner from '../Banner/Banner';
+
+function Home() {
+    return (
+        <div className='home'>
+            <h1>Home Component</h1>
+            <Banner />
+        </div>
+    )
+}
+
+export default Home;
